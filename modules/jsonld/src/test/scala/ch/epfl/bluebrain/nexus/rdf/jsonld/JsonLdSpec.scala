eb9561a985afcb6ca448c62e2c890de5404a0c1f
Wojtek Wajerowicz <wwajerowicz@users.noreply.github.com>
2020-01-15 17:27:04 +0100

    Move more json-ld utilities from the master branch. (#106)
    * Move more json-ld utilities from the master branch
    
    * Remove removeContextIris as it's no longer needed.

--- modules/jsonld/src/test/scala/ch/epfl/bluebrain/nexus/rdf/jsonld/JsonLdSpec.scala
+++ modules/jsonld/src/test/scala/ch/epfl/bluebrain/nexus/rdf/jsonld/JsonLdSpec.scala
@@ -1,11 +1,14 @@
 package ch.epfl.bluebrain.nexus.rdf.jsonld
 
 import cats.implicits._
+import ch.epfl.bluebrain.nexus.rdf.Iri.AbsoluteIri
 import ch.epfl.bluebrain.nexus.rdf.RdfSpec
 import ch.epfl.bluebrain.nexus.rdf.jsonld.JsonLd._
 import ch.epfl.bluebrain.nexus.rdf.jsonld.syntax._
 import ch.epfl.bluebrain.nexus.rdf.syntax.all._
+import io.circe.Json
 import io.circe.literal._
+import io.circe.syntax._
 
 import scala.util.{Failure, Success, Try}
 
@@ -311,73 +314,206 @@ class JsonLdSpec extends RdfSpec {
       }
     }
 
-    "return context value" when {
-      "json is an array" in {
-        json"""
-         [
-           {
-             "@context": {
-               "xsd": "http://www.w3.org/2001/XMLSchema#"
-             }
-           },
-           {
-             "@context": {
-               "schema": "http://schema.org/"
-             }
-           }
-         ]
-         """.contextValue shouldEqual
-          json"""
-          {
-            "xsd": "http://www.w3.org/2001/XMLSchema#",
-            "schema": "http://schema.org/"
-          }
-          """
+    val context: AbsoluteIri = url"https://bbp-nexus.epfl.ch/dev/v0/contexts/bbp/core/context/v0.1.0"
+
+    "injecting context" should {
+      val contextString = Json.fromString(context.show)
+
+      val mapping = List(
+        Json.obj("@id"        -> Json.fromString("foo-id"), "nxv:rev" -> Json.fromLong(1)) ->
+          Json.obj("@context" -> contextString, "@id"                 -> Json.fromString("foo-id"), "nxv:rev" -> Json.fromLong(1)),
+        Json.obj(
+          "@context" -> Json.fromString("http://foo.domain/some/context"),
+          "@id"      -> Json.fromString("foo-id"),
+          "nxv:rev"  -> Json.fromLong(1)
+        ) ->
+          Json.obj(
+            "@context" -> Json.arr(Json.fromString("http://foo.domain/some/context"), contextString),
+            "@id"      -> Json.fromString("foo-id"),
+            "nxv:rev"  -> Json.fromLong(1)
+          ),
+        Json.obj(
+          "@context" -> Json
+            .arr(
+              Json.fromString("http://foo.domain/some/context"),
+              Json.fromString("http://bar.domain/another/context")
+            ),
+          "@id"     -> Json.fromString("foo-id"),
+          "nxv:rev" -> Json.fromLong(1)
+        ) ->
+          Json.obj(
+            "@context" -> Json.arr(
+              Json.fromString("http://foo.domain/some/context"),
+              Json.fromString("http://bar.domain/another/context"),
+              contextString
+            ),
+            "@id"     -> Json.fromString("foo-id"),
+            "nxv:rev" -> Json.fromLong(1)
+          ),
+        Json.obj(
+          "@context" -> Json.obj(
+            "foo" -> Json.fromString("http://foo.domain/some/context"),
+            "bar" -> Json.fromString("http://bar.domain/another/context")
+          ),
+          "@id"     -> Json.fromString("foo-id"),
+          "nxv:rev" -> Json.fromLong(1)
+        ) ->
+          Json.obj(
+            "@context" -> Json.arr(
+              Json.obj(
+                "foo" -> Json.fromString("http://foo.domain/some/context"),
+                "bar" -> Json.fromString("http://bar.domain/another/context")
+              ),
+              contextString
+            ),
+            "@id"     -> Json.fromString("foo-id"),
+            "nxv:rev" -> Json.fromLong(1)
+          )
+      )
+
+      "properly add or merge context into JSON payload" in {
+        forAll(mapping) {
+          case (in, out) =>
+            in.addContext(context) shouldEqual out
+        }
+      }
+
+      "added context uri to empty context value" in {
+        val list = List(
+          Json.obj("@context" -> Json.obj()).addContext(context),
+          Json.obj("@context" -> Json.arr()).addContext(context),
+          Json.obj("@context" -> Json.fromString("")).addContext(context)
+        )
+        forAll(list)(_ shouldEqual Json.obj("@context" -> Json.fromString(context.asString)))
+      }
+
+      "be idempotent" in {
+        forAll(mapping) {
+          case (in, _) =>
+            in.addContext(context) shouldEqual in.addContext(context).addContext(context)
+        }
+      }
+
+      "extract context" in {
+        val context1          = jsonContentOf("/context/context1.json")
+        val context1Extracted = jsonContentOf("/context/context1_extracted.json")
+        context1.contextValue shouldEqual context1Extracted
+      }
+
+      "extract context with embedded contexts" in {
+        val context1          = jsonContentOf("/context/embedded-context.json")
+        val context1Extracted = jsonContentOf("/context/context1_extracted.json")
+        context1.contextValue shouldEqual context1Extracted
+      }
+
+      "extract context with embedded IRI contexts" in {
+        val context1 = jsonContentOf("/context/embedded-context-links.json")
+        context1.contextValue shouldEqual
+          Json.arr(
+            "http://example.com/1".asJson,
+            "http://example.com/2".asJson,
+            Json.obj("xsd" -> "http://www.w3.org/2001/XMLSchema#".asJson),
+            "http://example.com/3".asJson
+          )
+      }
+
+      "extract empty json when @context key missing" in {
+        Json.obj("one" -> Json.fromInt(1)).contextValue shouldEqual Json.obj()
+      }
+
+      "merge two contexts" in {
+        val context1 = jsonContentOf("/context/context1.json")
+        val context2 = jsonContentOf("/context/context2.json")
+
+        context1 mergeContext context2 shouldEqual jsonContentOf("/context/context_merged.json")
+      }
+
+      "append context" in {
+        val context1 = jsonContentOf("/context/context1.json")
+        val json     = context1 deepMerge Json.obj("one" -> Json.fromInt(1), "two" -> Json.fromInt(2))
+        val context2 = jsonContentOf("/context/context2.json")
+        val json2    = context2 deepMerge Json.obj("three" -> Json.fromInt(3), "four" -> Json.fromInt(4))
+
+        json appendContextOf json2 shouldEqual (jsonContentOf("/context/context_merged.json") deepMerge Json.obj(
+          "one" -> Json.fromInt(1),
+          "two" -> Json.fromInt(2)
+        ))
+      }
+
+      "append context when array" in {
+        val context1 = jsonContentOf("/context/context1-array.json")
+        val json     = context1 deepMerge Json.obj("one" -> Json.fromInt(1), "two" -> Json.fromInt(2))
+        val context2 = jsonContentOf("/context/context2.json")
+        val json2    = context2 deepMerge Json.obj("three" -> Json.fromInt(3), "four" -> Json.fromInt(4))
+        json appendContextOf json2 shouldEqual (jsonContentOf("/context/context_merged-array.json") deepMerge Json.obj(
+          "one" -> Json.fromInt(1),
+          "two" -> Json.fromInt(2)
+        ))
+      }
+
+      "replace context" in {
+        val context1 = jsonContentOf("/context/context1.json")
+        val json     = context1 deepMerge Json.obj("one" -> Json.fromInt(1), "two" -> Json.fromInt(2))
+        val context2 = Json.obj("@context" -> Json.obj("some" -> Json.fromString("context")))
+        val json2    = context2 deepMerge Json.obj("three" -> Json.fromInt(3), "four" -> Json.fromInt(4))
+
+        json replaceContext json2 shouldEqual
+          (Json.obj("one" -> Json.fromInt(1), "two" -> Json.fromInt(2)) deepMerge context2)
+      }
+
+      "replace context with iri" in {
+        val context1 = jsonContentOf("/context/context1.json")
+        val json     = context1 deepMerge Json.obj("one" -> Json.fromInt(1), "two" -> Json.fromInt(2))
+
+        json replaceContext context shouldEqual
+          Json.obj("@context" -> Json.fromString(context.asString), "one" -> Json.fromInt(1), "two" -> Json.fromInt(2))
       }
     }
-    "remove nested keys" when {
-      "json contains nested keys" in {
-        removeNestedKeys(
-          json"""
-          {
-            "@context": "http://example.com/1",
-            "field1": {
-              "@context": "http://example.com/2",
-              "field2": {
-                "field3": "Other text"
-              }
-            },
-            "field3": [
-              {
-                "@context": {
-                  "xsd": "http://www.w3.org/2001/XMLSchema#"
-                }
-              },
-              {
-                "@context": "http://example.com/3",
-                "field4": 2
-              }
-            ],
-            "field5": "Some text"
-          }
-         """,
-          "@context"
-        ) shouldEqual
-          json"""
-          {
-            "field1" : {
-              "field2" : {
-                "field3" : "Other text"
-              }
-            },
-            "field3" : [
-              {
-                "field4" : 2
-              }
-            ],
-            "field5" : "Some text"
-          }
-          """
+
+    "manipulating json" should {
+      "remove keys object" in {
+        val obj = Json.obj("one" -> Json.obj("two" -> Json.fromString("something")), "two" -> Json.fromString("abc"))
+        obj.removeKeys("two") shouldEqual Json.obj("one" -> Json.obj("two" -> Json.fromString("something")))
+        obj.removeKeys("three") shouldEqual obj
+      }
+
+      "remove the key from the json on nested fields" in {
+        val json     = jsonContentOf("/context/embedded-context-links.json")
+        val expected = jsonContentOf("/context/embedded-links-without-context.json")
+        json.removeNestedKeys("@context") shouldEqual expected
+      }
+
+      "remove keys array" in {
+        val obj      = Json.obj("one" -> Json.obj("two" -> Json.fromString("something")), "two" -> Json.fromString("abc"))
+        val objNoKey = Json.obj("one" -> Json.obj("two" -> Json.fromString("something")))
+        val arrObj   = Json.arr(obj, obj, Json.obj("three" -> Json.fromString("something")))
+        arrObj.removeKeys("two") shouldEqual Json.arr(
+          objNoKey,
+          objNoKey,
+          Json.obj("three" -> Json.fromString("something"))
+        )
+      }
+
+      "fetch @id aliases" in {
+        val json = jsonContentOf("/context/context-aliases.json")
+        json.contextAliases("@id") shouldEqual Set("id1", "id2")
+        json.contextAliases("@type") shouldEqual Set.empty[String]
+      }
+
+      "add @id" in {
+        val json = Json.obj("key" -> Json.fromString("value"))
+        val id   = url"http://example.com/id"
+        json.id(id) shouldEqual
+          Json.obj("key" -> Json.fromString("value"), "@id" -> Json.fromString(id.asString))
+
+        val jsonArr = Json.arr(json)
+        jsonArr.id(id) shouldEqual
+          Json.arr(Json.obj("key" -> Json.fromString("value"), "@id" -> Json.fromString(id.asString)))
+
+      }
+
+      "find top level @id in Json" in {
+        jsonContentOf("/context/simple-iri-context.json").id.rightValue shouldEqual url"http://nexus.example.com/john-doé"
       }
     }
   }
